e03d72fb05ec86d483a739ee0437f65fd1656a01
C05_Basem <basemhmoud735@gmail.com>
2021-05-14 15:53:52 +0300

    Return an article by a specific id

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 5000;
 
+app.use(express.json());
+
 const articles = [
   {
     id: 1,
@@ -24,10 +26,33 @@ const articles = [
     author: "Jouza",
   },
 ];
+
+//Return all the articles
+
 app.get("/articles", (req, res) => {
   res.status(200);
-  res.send(articles);
+  res.json(articles);
 });
+
+//Return an article by a specific id
+app.get("/articles/:id", (req, res) => {
+  const articlesID = req.params.id;
+  const found = articles.find((element) => {
+    return element.id === parseInt(articlesID);
+  });
+
+  if (found) {
+    // set the response status code to 200 (OK)
+    // sends back a response of the found user
+    res.status(200);
+    res.json(found);
+  } else {
+    // set the response status code to 404 (Not Found)
+    res.status(404);
+    res.json("articles not found");
+  }
+});
+
 app.listen(port, () => {
   console.log(`server run on port ${port}`);
 });
